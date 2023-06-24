27d399cfcf44f28e263e2805d6e019e1af820835
mendes-jv <mendes.silva.jv@gmail.com>
2023-06-24 05:26:09 -0300

    feat(putstr_fd): Implements lputstr_fd

---
+++ libft/sources/ft_lputstr_fd.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lputstr_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jovicto2 <jovicto2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/24 00:45:07 by jovicto2          #+#    #+#             */
+/*   Updated: 2023/06/24 04:48:04 by jovicto2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_lputstr_fd(char *s, int fd)
+{
+	size_t written_bytes;
+	
+	written_bytes = 0;
+	while (*s)
+		written_bytes += write(fd, s++, sizeof(char));
+	return(written_bytes);
+}
