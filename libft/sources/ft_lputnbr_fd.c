b0434041bb9bba57715230d3965c64e64e22770a
mendes-jv <mendes.silva.jv@gmail.com>
2023-06-24 22:14:23 -0300

    feature(libft): Implements ft_lputnbr

---
+++ libft/sources/ft_lputnbr_fd.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lputnbr_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jovicto2 <jovicto2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/24 19:08:06 by jovicto2          #+#    #+#             */
+/*   Updated: 2023/06/24 19:14:22 by jovicto2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_lputnbr_fd(long nbr, int fd)
+{
+	size_t	written_bytes;
+
+	written_bytes = 0;
+	if (nbr < 0)
+	{
+		nbr = -nbr;
+		written_bytes += write(fd, '-', sizeof(char));
+	}
+	if (nbr / 10 > 0)
+		written_bytes += ft_lputnbr_fd(nbr / 10, fd);
+	written_bytes += write(fd, nbr % 10 + '0', sizeof(char));
+	return (written_bytes);
+}
